e54595ddb15ffc726952529486b2c195fe1faa51
Gastón Acosta <gaston.1693@gmail.com>
2022-12-29 10:11:41 -0300

    tercera pre entrega final

---
+++ pre-entrega3-login.js
@@ -0,0 +1,51 @@
+const clientes = {
+        nombre: "Gastón Acosta",
+        edad: 29, 
+        nombreDeUsuario: "gasacosta",
+        contraseniaDeUsuario: "gastonAcosta123",
+        tieneTarjetaDeCredito: true,
+        resideEnArgentina: true
+    }
+
+const formLogin = document.querySelector("#login")
+const inputUser = document.querySelector("#input-user")
+const inputPass = document.querySelector("#input-pass")
+const logint = document.querySelector("#login-incorrect")
+const logout = document.querySelector("#logout")
+
+const subidaLocalStorage = (clave, valor) => {
+    localStorage.setItem(clave, JSON.stringify(valor))
+}
+
+const bajadaLocalStorage = (clave) => {
+    return JSON.parse(localStorage.getItem(clave))
+}
+
+formLogin.onsubmit = (event) => {
+    event.preventDefault()
+    if (inputUser.value === clientes.nombreDeUsuario && inputPass.value === clientes.contraseniaDeUsuario){
+    subidaLocalStorage("login", true)
+    formLogin.style.display = "none" 
+    } else {
+    logint.style.display = "block"
+    inputUser.style.border = "1px solid red"
+    inputPass.style.border = "1px solid red"
+    }
+}
+
+function checkLogin (clave) {
+    if ( clave !== true ) {
+        formLogin.style.display = "block"
+        logout.style.display = "none"
+    } else {
+        formLogin.style.display = "none"
+        logout.style.display = "block"
+    }
+}
+
+checkLogin(bajadaLocalStorage("login"))
+
+logout.onclick = () => {
+    localStorage.removeItem("login")
+    checkLogin(bajadaLocalStorage("login"))
+}
