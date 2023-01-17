7e52c91275dae2e7d6d329d0c1fa96bdf9f31aca
Gastón Acosta <gaston.1693@gmail.com>
2023-01-17 08:08:35 -0300

    cambios entrega final

--- pre-entrega3.js
+++ pre-entrega3.js
@@ -1,19 +1,35 @@
 const paquetes = [
     {
         nombreDelPaquete: "Paquete Básico",
-        precioDelPaquete: "$1000"
+        precioDelPaquete: "$1000",
+        accesoContenido: "Básico", 
+        cantidadDeDispositivos: 2,
+        calidadDeImagen: "720p",
+        reproduccionEnSimultaneo: false,
     },
     {
         nombreDelPaquete: "Paquete HD",
-        precioDelPaquete: "$2000"
+        precioDelPaquete: "$2000",
+        accesoContenido: "Medio",
+        cantidadDeDispositivos: 3,
+        calidadDeImagen: "1080p",
+        reproduccionEnSimultaneo: false,
     },
     {
         nombreDelPaquete: "Paquete 4K",
-        precioDelPaquete: "$3000"
+        precioDelPaquete: "$3000",
+        accesoContenido: "Avanzado",
+        cantidadDeDispositivos: 5,
+        calidadDeImagen: "1080p 4K",
+        reproduccionEnSimultaneo: true,
     },
     {
         nombreDelPaquete: "Paquete 4K Premium",
-        precioDelPaquete: "$4000"
+        precioDelPaquete: "$4000",
+        accesoContenido: "Premium",
+        cantidadDeDispositivos: 10,
+        calidadDeImagen: "2060p 4K",
+        reproduccionEnSimultaneo: true,
     }
 ]
 
